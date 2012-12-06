b853803ffc5ae9b44cf132210eb5981b1cc1e328
Dave Dopson <ddopson@gmail.com>
2012-12-06 11:02:38 -0800

    initial commit - Rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DdopsonPro::Application.config.secret_token = 'a994bdcc5e7228f83bc87f96b0a26b537f189fef15955e46fe0e21ae5e25da55a835d9aa616e7bc536f32436ac02e2938c1eddecdf0a433915e9352d5af92874'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DdopsonPro::Application.config.session_store :cookie_store, key: '_ddopson-pro_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DdopsonPro::Application.config.session_store :active_record_store
